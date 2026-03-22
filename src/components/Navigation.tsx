ae1594dfd9a3c7e78110b303d37c014c7e94376a
gabriel cajigas <gabrielcajigas@gabriels-Laptop.local>
2026-03-21 23:22:40 -0400

    chore: auto-lint, type-check, and format

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -42,7 +42,7 @@ export default function Navigation() {
           <img
             src="/logo.png"
             alt="LevelUp Local"
-            className="h-14 w-auto object-contain"
+            className="h-24 w-auto object-contain"
           />
         </Link>
 
